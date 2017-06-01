39131fbc9654d7c30e104786dd743595429c4a38
Geoffrey Sneddon <me@gsnedders.com>
2017-06-01 12:44:09 +0100

    Updating to 78d81defaca749cf49161af7a62fa21d7153b77a

--- clone.sh
+++ clone.sh
@@ -5,7 +5,7 @@ echo "Cloning GH repository"
 echo $DESTINATION
 echo $DESTINATION_BRANCH
 echo https://$GIT_NAME@github.com/$USERNAME/$REPO.git
-git clone -q https://$GIT_NAME:$GH_TOKEN@github.com/$USERNAME/$REPO.git $DESTINATION
+git clone --reference . https://$GIT_NAME:$GH_TOKEN@github.com/$USERNAME/$REPO.git $DESTINATION
 ls $DESTINATION
 cd $DESTINATION
 git branch
